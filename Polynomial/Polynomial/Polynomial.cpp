34ff465d187c182c8bc3bc35746978d89242b025
dec-gannii <rkdms1220@swu.ac.kr>
2022-01-17 17:07:25 +0900

    add remarks

--- Polynomial/Polynomial/Polynomial.cpp
+++ Polynomial/Polynomial/Polynomial.cpp
@@ -8,12 +8,12 @@
 #include "Polynomial.h"
 
 int main() {
-    Polynomial a, b, c;
-    a.read();
-    b.read();
-    c.add(a, b);
-    a.display("A = ");
-    b.display("B = ");
-    c.display("A + B = ");
+    Polynomial a, b, c; // 다항식 객체 a, b, c를 생성
+    a.read(); // 다항식 a의 차수와 각 차수에 따른 계수를 받아옴
+    b.read(); // 다항식 b의 차수와 각 차수에 따른 계수를 받아옴
+    c.add(a, b); // 다항식 a와 b를 더하는 연산 수행
+    a.display("A = "); // 다항식 a의 내용을 화면에 출력
+    b.display("B = "); // 다항식 b의 내용을 화면에 출력
+    c.display("A + B = ");  // 다항식 c(다항식 a와 다항식 b의 합)의 내용을 화면에 출력
     return 0;
 }
--- Polynomial/Polynomial/Polynomial.h
+++ Polynomial/Polynomial/Polynomial.h
@@ -17,42 +17,62 @@ public:
     // 다항식의 내용을 입력받는 멤버 함수
     void read() { // 다항식의 내용 입력
         printf("다항식의 최고 차수를 입력하시오 : ");
-        scanf("%d", &degree);
+        scanf("%d", &degree); // &degree에 해당하는 주소에 저장
         printf("각 항의 계수를 입력하시오. (총 %d개) : ", degree + 1);
+        // degree+1인 이유는 상수항을 포함하기 때문 -> 총 필요한 항의 계수의 갯수는 차수 + 1에 해당
         for ( int i = 0; i <= degree; i++ ) {
-            scanf("%f", coef+i);
+            scanf("%f", coef+i); // 배열의 순서를 증가시켜가며 해당하는 위치에 저장
         }
     }
     
-    void display(char *str=" Poly = ") {
+    // 다항식의 내용을 화면에 출력하는 함수
+    void display(char *str=" Poly = ") { // "Poly = "는 디폴트 매개변수에 해당
         printf("\t%s", str);
         for ( int i = 0; i < degree; i++ ) {
             printf("%5.1f x^%d + ", coef[i], degree - i);
+            // coef[i] : 계수
+            // degree - i : 차수
         }
-        printf( "%4.1f\n", coef[degree] );
+        printf( "%4.1f\n", coef[degree] ); // coef[degree] : 최고차항의 계수에 해당
     }
     
+    /*
+     디폴트 매개변수란?
+     매개변수를 받지 않은 경우 기본적으로 지정되는 매개변수
+     */
+    
+    // 다항식 a와 b를 더하는 함수. a와 b를 더해서 자신의 다항식을 설정
+    // 매개변수로 Polynomial 객체를 받음
     void add(Polynomial a, Polynomial b) {
-        if (a.degree > b.degree) {
-            *this = a;
-            for ( int i = 0; i <= b.degree; i++ ) {
+        if (a.degree > b.degree) { // 다항식 a의 항 > 다항식 b의 항
+            *this = a; // a 다항식을 자기 객체에 복사
+            for ( int i = 0; i <= b.degree; i++ ) { // b의 차수만큼 반복
                 coef[i+(degree-b.degree)] += b.coef[i];
+                // degree - b.degree : 차수의 차이
+                // b.coef[i] : b의 최고차항에서부터 i번째 항의 계수
             }
-        } else {
-            *this = b;
-            for ( int i = 0; i <= a.degree; i++ ) {
+        } else { // 다항식 a의 항 < 다항식 b의 항
+            *this = b; // b 다항식을 자기 객체에 복사
+            for ( int i = 0; i <= a.degree; i++ ) { // a의 차수만큼 반복
                 coef[i+(degree - a.degree)] += a.coef[i];
+                // degree - a.degree : 차수의 차이
+                // a.coef[i] : a의 최고차항에서부터 i번째 항의 계수
             }
         }
     }
+    /*
+     b.degree - a.degree 또는 a.degree - b.degree는 결과 다항식의 최대 차수가 됨
+     */
     
+    // 최고차수가 0인가?
     bool isZero() {
         return degree == 0;
     }
     
+    // 모든 계수의 부호를 바꿔줌
     void negate() {
         for ( int i = 0; i <= degree; i++ ) {
-            coef[i] = -coef[i];
+            coef[i] = -coef[i]; // 부호 변경
         }
     }
 };
